cda26ecc07766c7a05b98b9352bbb0dbf9edb35f
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-09-01 17:00:27 -0500

    Progress on the multi model.

--- src/controller/WorldMultiModelMDP.cpp
+++ src/controller/WorldMultiModelMDP.cpp
@@ -1,4 +1,6 @@
 #include "WorldMultiModelMDP.h"
+  
+const float WorldMultiModelMDP::MIN_MODEL_PROB = 0.001;
 
 WorldMultiModelMDP::WorldMultiModelMDP(boost::shared_ptr<RNG> rng, boost::shared_ptr<WorldModel> model, boost::shared_ptr<World> controller, boost::shared_ptr<AgentDummy> adhocAgent,const std::vector<std::vector<boost::shared_ptr<Agent> > > &agentModelList, const std::vector<float> &agentModelProbs, ModelUpdateType modelUpdateType):
   WorldMDP(rng,model,controller,adhocAgent),
@@ -14,6 +16,26 @@ void WorldMultiModelMDP::setState(const State_t &state) {
   WorldMDP::setState(state);
   selectModel();
 }
+
+void WorldMultiModelMDP::updateModels(const std::vector<Point2D> &lastPositions, Action::Type lastAction, const std::vector<Point2D> &currentPositions) {
+  // done if we're down to 1 model
+  if (agentModelProbs.size() == 1)
+    return;
+
+  std::vector<float> newModelProbs(agentModelProbs.size());
+
+  // TODO getNewModelProbs
+
+  // TODO check if all zero out
+  
+  // set our models
+  agentModelProbs.swap(newModelProbs);
+  // normalize the probabilities
+  normalizeModelProbs();
+  // delete models with very low probabilities
+  removeLowProbabilityModels();
+}
+
 
 void WorldMultiModelMDP::selectModel() {
   if (agentModelProbs.size() == 1)
@@ -36,4 +58,17 @@ void WorldMultiModelMDP::normalizeModelProbs() {
     total += agentModelProbs[i];
   for (unsigned int i = 0; i < agentModelProbs.size(); i++)
     agentModelProbs[i] /= total;
+}
+
+void WorldMultiModelMDP::removeLowProbabilityModels() {
+  unsigned int i = 0;
+  while (i < agentModelProbs.size()) {
+    if (agentModelProbs[i] < MIN_MODEL_PROB) {
+      // remove it from the probs and models
+      agentModelProbs.erase(agentModelProbs.begin()+i,agentModelProbs.begin()+i+1);
+      agentModelList.erase(agentModelList.begin()+i,agentModelList.begin()+i+1);
+    } else {
+      ++i;
+    }
+  }
 }
--- src/controller/WorldMultiModelMDP.h
+++ src/controller/WorldMultiModelMDP.h
@@ -13,14 +13,19 @@ public:
   WorldMultiModelMDP(boost::shared_ptr<RNG> rng, boost::shared_ptr<WorldModel> model, boost::shared_ptr<World> controller, boost::shared_ptr<AgentDummy> adhocAgent,const std::vector<std::vector<boost::shared_ptr<Agent> > > &agentModelList, const std::vector<float> &agentModelProbs, ModelUpdateType modelUpdateType);
   void setState(const State_t &state);
 
+  void updateModels(const std::vector<Point2D> &lastPositions, Action::Type lastAction, const std::vector<Point2D> &currentPositions);
+
 private:
   void selectModel();
   void normalizeModelProbs();
+  void removeLowProbabilityModels();
 
 private:
   std::vector<std::vector<boost::shared_ptr<Agent> > > agentModelList;
   std::vector<float> agentModelProbs;
   ModelUpdateType modelUpdateType;
+
+  const static float MIN_MODEL_PROB;
 };
 
 #endif /* end of include guard: WORLDMULTIMODELMDP_V3OI0GS4 */
