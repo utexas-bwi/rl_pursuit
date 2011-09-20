5c73dc6b1635491ae774ebd8af6074f9ddea78c9
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-09-20 15:02:16 -0500

    Handle printing results from multiple files for the same result.

--- bin/printResults.py
+++ bin/printResults.py
@@ -7,18 +7,36 @@ def loadResults(filename):
   with open(filename,'r') as f:
     reader = csv.reader(f)
     for row in reader:
-      iteration = row[0]
+      #iteration = row[0]
       stepsPerTrial = map(int,row[1:])
       numSteps.append(stepsPerTrial)
   numSteps = numpy.array(numSteps)
   return numSteps
 
-def main(filenames):
-  for filename in filenames:
-    print filename
-    numSteps = loadResults(filename)
-    print numSteps.mean()
+def main(filenames,sameResult):
+  if sameResult:
+    numSteps = None
+    for filename in filenames:
+      res = loadResults(filename)
+      if numSteps is None:
+        numSteps = res
+      else:
+        numSteps = numpy.hstack(numSteps,res)
+  else:
+    for filename in filenames:
+      print filename
+      numSteps = loadResults(filename)
+      print numSteps.mean()
 
 if __name__ == '__main__':
   import sys
-  main(sys.argv[1:])
+  startInd = 1
+  sameResult = False
+  if sys.argv[1] in ['-h','--help']:
+    print 'Usage: printResults.py [-a] result1.csv [result2.csv]'
+    print '  -a treats all the result files as if they come from a single result'
+    sys.exit(0)
+  if sys.argv[1] == '-a':
+    startInd = 2
+    sameResult = True
+  main(sys.argv[startInd:],sameResult)
