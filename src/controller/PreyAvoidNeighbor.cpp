5c6fbd6d072c8e4f8f63e849e7a0bb4c85842bc7
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-10-10 09:54:06 -0500

    PreyAvoidNeighbor - No move when not neighboring

--- src/controller/PreyAvoidNeighbor.cpp
+++ src/controller/PreyAvoidNeighbor.cpp
@@ -9,7 +9,7 @@ Modified: 2011-09-30
 #include "PreyAvoidNeighbor.h"
 #include <boost/lexical_cast.hpp>
 
-const float PreyAvoidNeighbor::noopWeight = 0.75;
+const float PreyAvoidNeighbor::noopWeight = 1.0;
 
 PreyAvoidNeighbor::PreyAvoidNeighbor(boost::shared_ptr<RNG> rng, const Point2D &dims):
   Agent(rng,dims)
