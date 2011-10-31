6a219f408319300237f36549b1b14e896833da62
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-10-31 17:02:32 -0500

    Run students in parallel.

--- bin/runStudents.py
+++ bin/runStudents.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, time, multiprocessing
 
-numTrials = 100
+def waitForProcesses(processes,numToStop):
+  while True:
+    for p in processes:
+      if p.poll() != None:
+        processes.remove(p)
+    if len(processes) <= numToStop:
+      return
+    time.sleep(1)
+
+numTrials = 1000
 
 with open('data/aamas11students.txt','r') as f:
   students = set(f.read().split())
@@ -10,20 +19,26 @@ with open('data/aamas11students.txt','r') as f:
 with open('configs/student_output.json','r') as f:
   config = f.read()
 
-with open('configs/trials-test.json','w') as f:
+trialConfig = 'configs/runStudents/trials.json'
+with open(trialConfig,'w') as f:
   f.write('{\n')
   f.write('"trials": %i\n' % numTrials)
   f.write('}\n')
 
 outputFilenames = []
+processes = []
 for student in students:
+  waitForProcesses(processes,multiprocessing.cpu_count()-1)
   contents = config.replace('data/aamas11students.txt',student)
   outputFilename = 'results/dtfile/%s.arff' % student
   outputFilenames.append(outputFilename)
   contents = contents.replace('"dtfile":""','"dtfile":"%s"'%outputFilename)
-  with open('configs/student-test.json','w') as f:
+  studentConfig = 'configs/runStudents/%s.json' % student
+  with open(studentConfig,'w') as f:
     f.write(contents)
-  subprocess.check_call(['bin/main','configs/student-test.json','configs/trials-test.json'])
+  p = subprocess.Popen(['bin/main',studentConfig,trialConfig])
+  processes.append(p)
+waitForProcesses(processes,0)
 
 combinedFilename = 'results/dtfile/combined.arff'
 out = open(combinedFilename,'w')
