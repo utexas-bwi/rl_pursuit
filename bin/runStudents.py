6724f842d3cfe80c34c80e155b57d790f33a4230
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-11-02 13:59:19 -0500

    Scripts for running weka.

--- bin/runStudents.py
+++ bin/runStudents.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess, time, multiprocessing
+import subprocess, time, multiprocessing, os
 
 def waitForProcesses(processes,numToStop):
   while True:
@@ -11,43 +11,54 @@ def waitForProcesses(processes,numToStop):
       return
     time.sleep(1)
 
-numTrials = 100
+def main(numTrials):
+  dtTrainDir = 'data/dt-train/%i' % numTrials
+  if not(os.path.exists(dtTrainDir)):
+    os.mkdir(dtTrainDir)
 
-with open('data/aamas11students.txt','r') as f:
-  students = set(f.read().split())
+  with open('data/aamas11students.txt','r') as f:
+    students = set(f.read().split())
 
-with open('configs/student_output.json','r') as f:
-  config = f.read()
+  with open('configs/student_output.json','r') as f:
+    config = f.read()
 
-trialConfig = 'configs/runStudents/trials.json'
-with open(trialConfig,'w') as f:
-  f.write('{\n')
-  f.write('"trials": %i\n' % numTrials)
-  f.write('}\n')
+  trialConfig = 'configs/runStudents/trials.json'
+  with open(trialConfig,'w') as f:
+    f.write('{\n')
+    f.write('"trials": %i\n' % numTrials)
+    f.write('}\n')
 
-outputFilenames = []
-processes = []
-for student in students:
-  waitForProcesses(processes,multiprocessing.cpu_count()-1)
-  contents = config.replace('data/aamas11students.txt',student)
-  outputFilename = 'results/dtfile/%s.arff' % student
-  outputFilenames.append(outputFilename)
-  contents = contents.replace('"dtfile":""','"dtfile":"%s"'%outputFilename)
-  studentConfig = 'configs/runStudents/%s.json' % student
-  with open(studentConfig,'w') as f:
-    f.write(contents)
-  p = subprocess.Popen(['bin/main',studentConfig,trialConfig])
-  processes.append(p)
-waitForProcesses(processes,0)
+  outputFilenames = []
+  processes = []
+  for student in students:
+    waitForProcesses(processes,multiprocessing.cpu_count()-1)
+    contents = config.replace('data/aamas11students.txt',student)
+    outputFilename = os.path.join(dtTrainDir,'%s.arff' % student)
+    outputFilenames.append(outputFilename)
+    contents = contents.replace('"dtfile":""','"dtfile":"%s"'%outputFilename)
+    studentConfig = 'configs/runStudents/%s.json' % student
+    with open(studentConfig,'w') as f:
+      f.write(contents)
+    p = subprocess.Popen(['bin/main',studentConfig,trialConfig])
+    processes.append(p)
+  waitForProcesses(processes,0)
 
-combinedFilename = 'results/dtfile/combined.arff'
-out = open(combinedFilename,'w')
-for i,outputFilename in enumerate(outputFilenames):
-  with open(outputFilename,'r') as f:
-    lines = f.readlines()
-  startLine = 0
-  if i != 0:
-    startLine = lines.index('@data\n') + 1 # + 1 to skip the data line
-  out.writelines(lines[startLine:])
+  combinedFilename = os.path.join(dtTrainDir,'combined.arff')
+  out = open(combinedFilename,'w')
+  for i,outputFilename in enumerate(outputFilenames):
+    with open(outputFilename,'r') as f:
+      lines = f.readlines()
+    startLine = 0
+    if i != 0:
+      startLine = lines.index('@data\n') + 1 # + 1 to skip the data line
+    out.writelines(lines[startLine:])
 
-out.close()
+  out.close()
+
+if __name__ == '__main__':
+  import sys
+  if (len(sys.argv) != 2) or (sys.argv[1] in ['-h','--help']):
+    print 'Usage: runStudents.py numTrialsPerStudent'
+    sys.exit(1)
+  numTrials = int(sys.argv[1])
+  main(numTrials)
---
+++ bin/weka/trainDT.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import subprocess, os
+from addARFFWeights import addARFFWeights
+
+BASE_PATH=os.path.join('bin','weka')
+
+def makeTemp(*args,**kwargs):
+  import tempfile
+  fd,temp = tempfile.mkstemp(*args,**kwargs)
+  os.close(fd)
+  return temp
+
+def wekaCommandPrefix():
+  return ['java','-cp',os.path.join(BASE_PATH,'weka.jar')]
+
+def removeTrialStep(inFile,outFile):
+  cmd = wekaCommandPrefix() + ['weka.filters.unsupervised.attribute.Remove','-R','1-2','-i',inFile,'-o',outFile]
+  subprocess.check_call(cmd)
+
+def createTree(inFile,outFile,options):
+  cmd = wekaCommandPrefix() + ['weka.classifiers.trees.REPTree','-t',inFile,'-i'] + options
+  subprocess.check_call(cmd,stdout=open(outFile,'w'))
+
+def extractTree(inFile,outFile):
+  with open(inFile,'r') as f:
+    lines = f.readlines()
+  ind = lines.index('REPTree\n')
+  startInd = ind + 3 # 3 lines later, including REPTree
+  endInd = startInd + lines[startInd:].index('\n')
+  with open(outFile,'w') as f:
+    f.writelines(lines[startInd:endInd])
+
+def weightTree(inFile,dataFile,outFile):
+  if os.uname()[4] == 'x86_64':
+    arch = '64'
+  else:
+    arch = '32'
+  cmd = [os.path.join(BASE_PATH,'addWeights%s' % arch),inFile,dataFile]
+  subprocess.check_call(cmd,stdout=open(outFile,'w'))
+
+def main(inFile,basename,stayWeight=None,treeOptions=[]):
+  descFile = os.path.join('data','dt','desc','%s.desc' % basename)
+  unweightedFile = os.path.join('data','dt','unweighted','%s.weka' % basename)
+  weightedFile = os.path.join('data','dt','weighted','%s.weka' % basename)
+
+  # create the temporary files we need
+  tmpData = makeTemp('.arff')
+  try:
+    print 'Removing trial and step features'
+    removeTrialStep(inFile,tmpData)
+    if (stayWeight is not None) and (abs(stayWeight - 1.0) > 0.0001):
+      print 'Adding stay weights'
+      addARFFWeights(tmpData,tmpData,stayWeight)
+    print 'Running weka to create initial tree'
+    createTree(tmpData,descFile,treeOptions)
+    print 'Extracting tree from weka output'
+    extractTree(descFile,unweightedFile)
+    print 'Adding class weights to tree'
+    weightTree(unweightedFile,tmpData,weightedFile)
+    print 'Done.'
+  finally:
+    os.remove(tmpData)
+
+if __name__ == '__main__':
+  import sys
+  args = sys.argv[1:]
+  usage = 'Usage trainDT.py inFile basename [treeOptions ...]'
+  if len(args) < 2:
+    print usage
+    sys.exit(1)
+  if args[0] in ['-h','--help']:
+    print usage
+    sys.exit(0)
+    
+  inFile = args[0]
+  basename = args[1]
+  stayWeight = None
+  treeOptions = args[2:]
+
+  main(inFile,basename,stayWeight,treeOptions)
