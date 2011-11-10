40573fd1662f74e834935da8989339ef50204ed5
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-11-10 11:22:03 -0600

    Simulate recovery

--- bin/recoverCondor.py
+++ bin/recoverCondor.py
@@ -15,21 +15,40 @@ def runJob(directory,jobNum,recoveryDir,contents):
     f.write(contents)
   subprocess.check_call(['condor_submit',jobFile])
 
-def main(directory,numJobs):
+def main(directory,numJobs,simulate):
   recoveryDir = os.path.join(directory,'recovery')
   if not(os.path.exists(recoveryDir)):
-    os.mkdir(recoveryDir)
+    if simulate:
+      print 'Would make dir: %s' % recoveryDir
+    else:
+      os.mkdir(recoveryDir)
   with open(os.path.join(directory,'job.condor')) as f:
     contents = f.read()
+  if simulate:
+    print 'Would run jobs:',
   for i in range(numJobs):
     if isJobComplete(directory,i):
       continue
-    runJob(directory,i,recoveryDir,contents)
+    if simulate:
+      print i,
+    else:
+      runJob(directory,i,recoveryDir,contents)
+  print ''
 
 if __name__ == '__main__':
   import sys
   args = sys.argv[1:]
-  usage = 'recoverCondor.py directory numJobs'
+  usage = 'recoverCondor.py [options] directory numJobs'
+  usage += '\nOptions:'
+  usage += '\n  -s --simulate - only simulate, don\'t start the jobs or create the files'
+  simulate = False
+  if '-s' in args:
+    args.remove('-s')
+    simulate = True
+  if '--simulate' in args:
+    args.remove('--simulate')
+    simulate = True
+
   if args[0] in ['-h','--help']:
     print usage
     sys.exit(0)
@@ -38,4 +57,4 @@ if __name__ == '__main__':
     sys.exit(1)
   directory = args[0]
   numJobs = int(args[1])
-  main(directory,numJobs)
+  main(directory,numJobs,simulate)
