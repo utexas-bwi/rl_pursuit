c387dee15168f620e80e1092b8b65daf2b25df44
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-12-02 10:32:23 -0600

    Removed unnecessary dependency.

--- targets/buildDT.mk
+++ targets/buildDT.mk
@@ -1,1 +1,1 @@
-buildDT_SOURCES := $(addprefix $(SOURCE_DIR)/, main/buildDT.cpp learning/DecisionTree.cpp common/Point2D.cpp learning/ArffReader.cpp learning/Common.cpp)
+buildDT_SOURCES := $(addprefix $(SOURCE_DIR)/, main/buildDT.cpp learning/DecisionTree.cpp learning/ArffReader.cpp learning/Common.cpp)
