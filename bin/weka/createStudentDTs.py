69cae8623d64cc73db5708202495d841803cba65
Samuel Barrett <sbarrett@cs.utexas.edu>
2011-12-13 12:04:37 -0600

    Take in use weka parameter

--- bin/weka/createStudentDTs.py
+++ bin/weka/createStudentDTs.py
@@ -3,7 +3,7 @@
 import os
 from createDT import main as createDT
 
-def main(basename,stayWeight,treeOptions):
+def main(basename,stayWeight,treeOptions,useWeka):
   with open('data/students.txt','r') as f:
     students = set(f.read().split())
 
@@ -13,12 +13,16 @@ def main(basename,stayWeight,treeOptions):
     print student
     print '-------------------'
     dataFile = os.path.join(dataDir,'%s.arff'%student)
-    createDT(dataFile,'%s-%s'%(basename,student),stayWeight,treeOptions)
+    createDT(dataFile,'%s-%s'%(basename,student),stayWeight,treeOptions,useWeka)
 
 if __name__ == '__main__':
   import sys
-  usage = 'Usage: createStudentDTs.py basename [treeOptions ...]'
+  usage = 'Usage: createStudentDTs.py basename [--weka] [treeOptions ...]'
   args = sys.argv[1:]
+  useWeka = False
+  if '--weka' in args:
+    args.remove('--weka')
+    useWeka = True
   if len(args) < 1:
     print usage
     sys.exit(1)
@@ -28,4 +32,4 @@ if __name__ == '__main__':
   basename = args[0]
   stayWeight = None
   treeOptions = args[1:]
-  main(basename ,stayWeight,treeOptions)
+  main(basename ,stayWeight,treeOptions,useWeka)
