0eb375c4f59df62064e2e1e3d83c852f2a227b79
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-02-03 19:01:40 -0600

    Adding perturbation files.

---
+++ bin/generatePerturbations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import random
+
+predsAreSame = False
+numPerturbations = 1000
+types = ['noop','random']
+#types = ['random']
+#types = ['noop']
+amountMin = 0.0
+amountMax = 0.2
+numPreds = 3
+
+for i in range(numPerturbations):
+  for j in range(numPreds):
+    if not(predsAreSame) or (j == 0):
+      t = random.choice(types)
+      amount = random.uniform(amountMin,amountMax)
+    print t,amount,
+  print ''
