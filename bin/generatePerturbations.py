1bf9dc45cd645debbdc4639f079a1fe552eb3f63
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-02-03 19:15:39 -0600

    Changes to perturbations - 4 preds not 3

--- bin/generatePerturbations.py
+++ bin/generatePerturbations.py
@@ -2,14 +2,14 @@
 
 import random
 
-predsAreSame = False
+predsAreSame = True
 numPerturbations = 1000
-types = ['noop','random']
-#types = ['random']
+#types = ['noop','random']
+types = ['random']
 #types = ['noop']
 amountMin = 0.0
 amountMax = 0.2
-numPreds = 3
+numPreds = 4
 
 for i in range(numPerturbations):
   for j in range(numPreds):
