89922cba832a3f4e7d02785d6a2bb529345805fb
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-02-13 17:56:41 -0600

    Independent transfer.

---
+++ bin/weka/createTransferDTsIndependent.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import subprocess, os
+from common import getUniqueStudents, getArch
+
+studentFile = 'data/newStudents29.txt'
+
+def main(targetDir,sourceDir,prefix,jobInd):
+  students = getUniqueStudents(studentFile)
+  i = -1
+  for targetStudent in students:
+    for sourceStudent in students:
+      if (targetStudent == sourceStudent):
+        continue
+      i += 1
+      if (jobInd is not None) and (i != jobInd):
+        continue
+      print targetStudent, sourceStudent
+      cmd = ['bin/%s/boostIndependent' % getArch(),targetStudent,sourceStudent,targetDir,sourceDir]
+      descFile = os.path.join(targetDir,'boostIndependent',targetStudent + '-' + sourceStudent + '.desc')
+      subprocess.check_call(cmd,stdout=open(descFile,'w'))
+
+def extractTree(inFile,outFile):
+  with open(inFile,'r') as f:
+    lines = f.readlines()
+  startInd = [x.startswith('@relation') for x in lines].index(True)
+  ignoreStart = lines.index('REPTree\n')
+  ignoreEnd = ignoreStart + 3 # 3 lines later, including REPTree
+  endInd = [x.startswith('Size of the tree : ') for x in lines].index(True) - 1
+  with open(outFile,'w') as f:
+    f.writelines(lines[startInd:ignoreStart])
+    f.writelines(lines[ignoreEnd:endInd])
+
+if __name__ == '__main__':
+  import sys
+  usage = 'Usage: createTransferTrees.py targetDir sourceDir prefix [--only NUM]'
+  args = sys.argv[1:]
+  jobInd = None
+  if ('-h' in args) or ('--help' in args):
+    print usage
+    sys.exit(0)
+  if '--only' in args:
+    ind = args.index('--only')
+    jobInd = int(args[ind+1])
+    args = args[:ind] + args[ind+2:]
+  if len(args) != 3:
+    print usage
+    sys.exit(1)
+  targetDir = args[0]
+  sourceDir = args[1]
+  prefix = args[2]
+  main(targetDir,sourceDir,prefix,jobInd)
+
