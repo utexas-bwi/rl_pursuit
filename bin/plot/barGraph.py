1a9c6de438c114e28743b978bb858fe76c50553b
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-02-16 21:23:23 -0600

    Better plotting.

--- bin/plot/barGraph.py
+++ bin/plot/barGraph.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# NOTE: for better hatching, hack /usr/lib/pymodules/python2.7/matplotlib/backends/backend_ps.py
+#  change line: "0 setlinewidth" to "1 setlinewidth"
+
 import sys, numpy
 import matplotlib.pyplot as plt
 
@@ -22,41 +25,49 @@ def parseOptions(dest,defaults,options,labelForErrors = None):
       val = defaults[key]
     dest.__dict__[key] = val
 
-class Bar(object):
-  defaults = {
-    'label': 'Unknown'
-  }
-  def __init__(self,value,**kwargs):
-    self.value = value
-    parseOptions(self,self.defaults,kwargs,'Bar')
-
-def makeBarGraph(bars,options):
-  vals = [x.value for x in bars]
+def getAxisBounds(values,errors,yMinFixed):
+  if errors is None:
+    vals = values
+  else:
+    vals =  [v+e for v,e in zip(values,errors)]
+    vals += [v-e for v,e in zip(values,errors)]
   yMin = min(vals)
   yMax = max(vals)
-  yMin -= (yMax - yMin) * 0.1
-  yMax += (yMax - yMin) * 0.1
-  xMin = 0 - 0.1
-  xMax = len(bars) - 0.1
+  yFudge = (yMax - yMin) * 0.1
+  yMin -= yFudge
+  yMax += yFudge
+  xFudge = 0.5
+  xMin = -xFudge
+  xMax = len(values) - xFudge
+  if yMinFixed is not None:
+    yMin = yMinFixed
+  return [xMin,xMax,yMin,yMax]
 
-  fontSize = 12
-  tickSize = 12
 
-  params = {'backend': 'agg','axes.labelsize': fontSize,'text.fontsize': fontSize,'legend.fontsize': fontSize,'xtick.labelsize': tickSize,'ytick.labelsize': tickSize,'text.usetex': True, 'ps.usedistiller': 'xpdf'}
-  #if options.filename is not None:
-  params['backend'] = 'PS'
+def makeBarGraph(values,options):
+
+  fontSize = options.fontSize
+  tickSize = fontSize
+
+  params = {'backend': 'PS','axes.labelsize': fontSize,'text.fontsize': fontSize,'legend.fontsize': fontSize,'xtick.labelsize': tickSize,'ytick.labelsize': tickSize,'text.usetex': True, 'ps.usedistiller': 'xpdf'}
   plt.rcParams.update(params)
 
   plt.figure()
-  plt.axis([xMin,xMax,yMin,yMax])
-  for x,bar in enumerate(bars):
-    plt.bar(x,bar.value,label=bar.label)
-  plt.legend()
+  bounds = getAxisBounds(values,options.errors,options.yMinFixed)
+  plt.axis(bounds)
+  for x,v in enumerate(values):
+    plt.bar(x-0.4,v,label=options.labels[x],color=options.colors[x],hatch=options.styles[x],yerr=options.errors[x],ecolor='black')
+  plt.xticks([])
+  plt.legend(loc=options.legendLoc)
+  
+  plt.ylabel(options.ylabel)
+  plt.xlabel(options.xlabel)
+  plt.title(options.title)
   
   if options.filename is None:
     plt.show()
   else:
-    plt.savefig(options.filename)
+    plt.savefig(options.filename,format='eps',bbox_inches='tight',pad_inches=0.1)
 
 def readFile(filename):
   data = numpy.loadtxt(filename,dtype=int,delimiter=',')
@@ -68,16 +79,31 @@ def main(filenames,options):
   bars = []
   for filename in filenames:
     vals = readFile(filename)
-    bars.append(Bar(vals.mean(),label=filename))
+    bars.append(vals.mean())
+    if options.errors is not None:
+      options.errors.append(numpy.std(vals) / numpy.sqrt(len(vals)))
   makeBarGraph(bars,options)
+
+def getMainOpts(**kwargs):
+  defaults = {
+    'filename': None,
+    'yMinFixed': None,
+    'labels': None,
+    'errors': [],
+    'colors': ['b','r','g','c','m','y','k'],
+    'styles': ['.','|','-','/','\\','+','x','o','O','*'],
+    'legendLoc': 'best',
+    'ylabel': '',
+    'xlabel': '',
+    'title': '',
+    'fontSize': 24
+  }
+  options = Options()
+  parseOptions(options,defaults,kwargs,'Main')
+  return options
 
 if __name__ == '__main__':
   args = sys.argv[1:]
-  clo = {}
-  defaults = {
-    'filename': None
-  }
-  options = Options()
-  parseOptions(options,defaults,clo,'Main')
   assert(len(args) > 0)
+  options = getMainOpts(labels=args)
   main(args,options)
