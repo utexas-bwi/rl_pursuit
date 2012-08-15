b74f1e5cb114f88d4bbaab2dac8ed4b4f4e03260
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-08-15 13:25:57 -0500

    Run student with hc planning

---
+++ bin/runStudentCrossHCPlan.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import sys, os, subprocess
+from weka.common import getUniqueStudents, makeTemp
+
+trialsPerStudent = 100
+trialsPerJob = 10
+dirName = 'condor/matrix'
+
+def getRunInfo(jobNum,trialsPerJob):
+  students = getUniqueStudents('data/newStudents29.txt')
+  models = ['gr','ta','gp','pd']
+  #models = students
+  jobsPerStudent = trialsPerStudent / trialsPerJob
+  temp = jobNum
+  numStudents = len(students)
+  numModels = len(models)
+  trialNumber = temp % jobsPerStudent
+  temp /= jobsPerStudent
+  studentModel = models[temp % numModels]
+  temp /= numModels
+  if temp > numStudents:
+    sys.exit(3)
+  student = students[temp]
+  return trialNumber,student,studentModel
+
+jobNum = int(sys.argv[1])
+
+baseConfig = '''
+{
+  "planner": {
+    "weighted": true, 
+    "theoreticallyCorrectLambda": false, 
+    "models": [
+      {
+        "prey": "random", 
+        "predator": "$(MODEL_STUDENT)",
+        "desc": "$(MODEL_STUDENT)"
+      }
+
+    ], 
+    "playouts": 1000, 
+    "initialStateVisits": 0, 
+    "rewardBound": 0.5, 
+    "update": "polynomial", 
+    "initialStateActionVisits": 0, 
+    "unseenValue": 999999, 
+    "pruningMemory": -1, 
+    "depth": 100, 
+    "initialValue": 0, 
+    "students": "data/newStudents29.txt", 
+    "student": "$(MODEL_STUDENT)", 
+    "time": 0, 
+    "silver": false, 
+    "gamma": 0.95, 
+    "lambda": 0.8
+  }, 
+  "verbosity": {
+    "stepsPerEpisode": false, 
+    "stepsPerTrial": false, 
+    "summary": true, 
+    "description": false, 
+    "observation": false
+  }, 
+  "predator": "student", 
+  "adhoc": "mcts", 
+  "height": 20, 
+  "width": 20, 
+  "prey": "random", 
+  "predatorOptions": {
+    "student": "$(STUDENT)"
+  }, 
+  "centerPrey": true,
+  "maxNumStepsPerEpisode": 500,
+  "trials": $(TRIALS_PER_STUDENT),
+  "trialsPerJob": $(TRIALS_PER_JOB),
+  "save": {"results":"$(RESULT_DIR)/$(JOBNUM).csv"},
+  "dir": "$(DIR)"
+}
+
+'''
+
+trialNumber,student,studentModel = getRunInfo(jobNum,trialsPerJob)
+
+resultDir = '%s/results/%s/%s' % (dirName,student,studentModel)
+if not(os.path.exists(resultDir)):
+  os.makedirs(resultDir)
+# make the config
+config = baseConfig
+config = config.replace('$(DIR)',dirName)
+config = config.replace('$(TRIALS_PER_STUDENT)',str(trialsPerStudent))
+config = config.replace('$(TRIALS_PER_JOB)',str(trialsPerJob))
+config = config.replace('$(STUDENT)',student)
+config = config.replace('$(MODEL_STUDENT)',studentModel)
+config = config.replace('$(RESULT_DIR)',resultDir)
+configFilename = makeTemp()
+try:
+  with open(configFilename,'w') as f:
+    f.write(config)
+  # run it
+  cmd = ['bin/main',str(trialNumber),configFilename]
+  subprocess.check_call(cmd)
+finally:
+  os.remove(configFilename)
