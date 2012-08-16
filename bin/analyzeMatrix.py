22e02450037b0fce60915f688fdf8df5c186624c
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-08-16 10:30:08 -0500

    Improvements to matrix scripts

--- bin/analyzeMatrix.py
+++ bin/analyzeMatrix.py
@@ -1,62 +1,67 @@
 #!/usr/bin/env python
 
 import sys, csv, itertools, numpy, scipy
+from optparse import OptionParser
+from genMatrix import addMetricParser, getMetric
 
-filename = sys.argv[1]
-numStudentsToChoose = 4
-useHC = True
-evalFunc = numpy.mean
-#evalFunc = numpy.max
+validMetrics = [numpy.mean,numpy.max]
 
-def calcVal(results,current):
+def calcVal(results,current,metric):
   vals = results[:,current]
   mins = numpy.min(vals,axis=1)
-  return evalFunc(mins)
+  return metric(mins)
 
-def calcBest(results,models):
+def calcBest(results,models,numStudentsToChoose,metric):
   best = None
   bestVal = numpy.inf
   for current in itertools.combinations(range(len(models)),numStudentsToChoose):
-    val = calcVal(results,current)
+    val = calcVal(results,current,metric)
     if val < bestVal:
       best = current
       bestVal = val
   return bestVal,best,[models[x] for x in best]
 
+def main(filename,metric,opts):
+  reader = csv.reader(open(filename,'r'),delimiter=',')
+  reader.next() # ignore first line
+  header = reader.next()
+  origModels = header[1:]
+  students = origModels[:-4]
+  if opts.useHC:
+    models = list(origModels)
+  else:
+    models = list(students)
 
-reader = csv.reader(open(filename,'r'),delimiter=',')
-reader.next() # ignore first line
-header = reader.next()
-origModels = header[1:]
-students = origModels[:-4]
-if useHC:
-  models = list(origModels)
-else:
-  models = list(students)
+  results = numpy.zeros([len(students),len(models)])
+  for i,row in enumerate(reader):
+    if len(origModels) != len(row) - 1:
+      print 'Bad Size:',len(origModels),len(row)-1
+      sys.exit(2)
+    for j,v in enumerate(row[1:len(models)+1]):
+      results[i,j] = float(v)
 
-results = numpy.zeros([len(students),len(models)])
-numExpectedCols = len(models)
-for i,row in enumerate(reader):
-  if len(origModels) != len(row) - 1:
-    print 'Bad Size:',len(origModels),len(row)-1
-    sys.exit(2)
-  for j,v in enumerate(row[1:len(models)+1]):
-    results[i,j] = float(v)
-  #results[row[0]] = {}
-  #for k,v in zip(header[1:],row[1:]):
-    #results[row[0]][k] = float(v)
+  bestVal,bestInds,bestModels = calcBest(results,models,opts.numStudentsToChoose,metric)
+  #print 'Overall best: %s %s' % (bestVal,bestModels)
+  print 'Overall,%s' % (','.join(bestModels))
+  for i,student in enumerate(students):
+    tempResults = scipy.delete(results,i,0)
+    tempResults = scipy.delete(tempResults,i,1)
+    tempModels = list(models)
+    del tempModels[i]
+    bestVal,bestInds,bestModels = calcBest(tempResults,tempModels,opts.numStudentsToChoose,metric)
+    #print 'Leave one out %s: %s %s' % (student,bestVal,bestModels)
+    print '%s,%s' % (student,','.join(bestModels))
+    sys.stdout.flush()
 
-#print results
-
-bestVal,bestInds,bestModels = calcBest(results,models)
-#print 'Overall best: %s %s' % (bestVal,bestModels)
-print 'Overall,%s' % (','.join(bestModels))
-for i,student in enumerate(students):
-  tempResults = scipy.delete(results,i,0)
-  tempResults = scipy.delete(tempResults,i,1)
-  tempModels = list(models)
-  del tempModels[i]
-  bestVal,bestInds,bestModels = calcBest(tempResults,tempModels)
-  #print 'Leave one out %s: %s %s' % (student,bestVal,bestModels)
-  print '%s,%s' % (student,','.join(bestModels))
-  sys.stdout.flush()
+if __name__ == '__main__':
+  parser = OptionParser('%prog [options] inCSV')
+  addMetricParser(parser,validMetrics)
+  parser.add_option('--hc',action='store_true',dest='useHC',default=True)
+  parser.add_option('--no-hc',action='store_false',dest='useHC',default=True)
+  parser.add_option('-n','--numToChoose',action='store',dest='numStudentsToChoose',default=4,type='int')
+  
+  opts,args = parser.parse_args()
+  assert(len(args) == 1),'Incorrect number of args, expected 1'
+  filename = args[0]
+  metric = getMetric(opts,validMetrics,passOpts=False)
+  main(filename,metric,opts)
--- bin/genMatrix.py
+++ bin/genMatrix.py
@@ -3,28 +3,64 @@
 import numpy
 from weka.common import getUniqueStudents
 from printResults import loadResults
+from optparse import OptionParser
 
-dirName = 'condor/matrix'
-students = getUniqueStudents()
-#models = list(students)
-models = list(students) + ['gr','ta','gp','pd']
-resultDir = '%s/results/%s/%s'
-metric = numpy.mean
-#metric = len
-#metric = lambda x: numpy.sum(x == 501) / float(len(x))
-#def metric(x):
-  #if (numpy.sum(x == 501) == 0):
-    #return numpy.mean(x)
-  #else:
-    #return numpy.inf
+def mean(_,x):
+  return numpy.mean(x)
 
-print 'Columns are planning model'
-print ',' + ','.join(models)
+def meanPenalized(opts,x):
+  return numpy.mean(x) + opts.penalty * numpy.sum(x == opts.maxLength)
 
-for student in students:
-  s = '%s,' % student
-  for model in models:
-    currResultDir = resultDir % (dirName,student,model)
-    results = loadResults(currResultDir)
-    s += '%s,' % metric(results)
-  print s[:-1]
+def meanInf(opts,x):
+  if (numpy.sum(x == opts.maxLength) == 0):
+    return numpy.mean(x)
+  else:
+    return numpy.inf
+
+def fracFinished(opts,x):
+  return 1.0 - numpy.sum(x == opts.maxLength) / float(len(x))
+
+validMetrics = [mean,meanPenalized,meanInf,fracFinished]
+
+def main(metric):
+  dirName = 'condor/matrix'
+  students = getUniqueStudents()
+  #models = list(students)
+  models = list(students) + ['gr','ta','gp','pd']
+  resultDir = '%s/results/%s/%s'
+
+  print 'Columns are planning model'
+  print ',' + ','.join(models)
+
+  for student in students:
+    s = '%s,' % student
+    for model in models:
+      currResultDir = resultDir % (dirName,student,model)
+      results = loadResults(currResultDir)
+      s += '%s,' % metric(results)
+    print s[:-1]
+
+def addMetricParser(parser,validMetrics):
+  validMetricNames = [x.__name__ for x in validMetrics]
+  parser.add_option('-m','--metric',action='store',dest='metric',type='str',default='mean',help='metric to evaluate runs, options are: %s' % ' '.join(validMetricNames))
+
+def getMetric(opts,validMetrics,passOpts=True):
+  validMetricNames = [x.__name__ for x in validMetrics]
+  assert(opts.metric in validMetricNames),'Invalid metric: %s, allowed set: %s' % (opts.metric,' '.join(validMetricNames))
+  metricInd = validMetricNames.index(opts.metric)
+  if passOpts:
+    metric = lambda x: validMetrics[metricInd](opts,x)
+  else:
+    metric = validMetrics[metricInd]
+  return metric
+
+if __name__ == '__main__':
+  parser = OptionParser('%prog [options]')
+  addMetricParser(parser,validMetrics)
+  parser.add_option('--maxLength',action='store',dest='maxLength',type='int',default=501,help='max length of an episode')
+  parser.add_option('--penalty',action='store',dest='penalty',type='int',default=1000,help='penalty for an unfinished episode')
+
+  opts,args = parser.parse_args()
+  assert(len(args) == 0),'Incorrect number of args'
+  metric = getMetric(opts,validMetrics)
+  main(metric)
