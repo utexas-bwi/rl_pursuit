faec56c570762f4051c8ec864cc889a528838673
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-08-22 14:47:22 -0500

    Create HCDTs.

---
+++ bin/weka/createHCDTs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from createDT import main as createDT
+from common import getUniqueStudents, getFilename, TRAIN, parseArgs
+
+def main(basename,dataDir,stayWeight,treeOptions,options):
+  students = ['gr','ta','gp','pd']
+  
+  for i,student in enumerate(students):
+    if (options.studentInd is not None) and (i != options.studentInd):
+      continue
+    print '-------------------'
+    print student
+    print '-------------------'
+    dataFile = getFilename(dataDir,student,TRAIN)
+    createDT(dataFile,basename,'only-%s' % student,stayWeight,treeOptions,options.useWeka,options.numInstances,options.randomTree,options.numRandomTrees,options.featureFrac,options.resampleFrac,options.randomTreeInd)
+
+if __name__ == '__main__':
+  from optparse import make_option
+  opts = []
+  opts.append(make_option('--only',action='store',dest='studentInd',default=None,help='only run for specified student',metavar='NUM',type='int'))
+  opts.append(make_option('--randomTreeInd',action='store',dest='randomTreeInd',default=None,help='only run for random tree ind',metavar='NUM',type='int'))
+  opts.append(make_option('--dataDir',action='store',dest='dataDir',default=None,help='data dir',metavar='DIR'))
+
+  opts.append(make_option('--random',action='store_true',dest='randomTree',default=False))
+  opts.append(make_option('--featureFrac',action='store',dest='featureFrac',default=0.8,type='float'))
+  opts.append(make_option('--numRandomTrees',action='store',dest='numRandomTrees',default=10,type='int'))
+  opts.append(make_option('--resampleFrac',action='store',dest='resampleFrac',default=0.5,type='float'))
+
+  usage = 'Usage: createStudentDTs.py [options] baseDir [-- treeOptions ...]'
+  options,args,treeOptions = parseArgs(usage=usage,numArgs=1,studentOptions=False,options=opts,numInstancesFlag=True)
+  baseDir = args[0]
+  if options.dataDir is None:
+    options.dataDir = baseDir
+  stayWeight = None
+  main(baseDir,options.dataDir,stayWeight,treeOptions,options)
