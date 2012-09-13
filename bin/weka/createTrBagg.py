f65b8f7fbb5d5fb59e4af4afa2fe053d076793a9
Samuel Barrett <sbarrett@cs.utexas.edu>
2012-09-13 14:27:59 -0500

    trainClassifier handles fracSourceData TrBagg allows for balancing the prob between target and source

--- bin/weka/createTrBagg.py
+++ bin/weka/createTrBagg.py
@@ -4,8 +4,23 @@ import sys, random, os
 from common import getUniqueStudents, makeTemp, getFilename, TRAIN, DESC
 from createDT import resample, makeTree #, removeTrialStep
 
+def calcProbs(students,numSource,numTarget,studentInd):
+  numInstances = (len(students) - 1) * numSource + numTarget
+  probs = [numSource / float(numInstances) for s in students]
+  probs[studentInd] = numTarget / float(numInstances)
+  return probs
+
+def calcBalancedProbs(students,numSource,numTarget,studentInd):
+  probs = [0.5 / (len(students) - 1) for s in students]
+  probs[studentInd] = 0.5
+  return probs
+
 def main(args=sys.argv[1:]):
-  usage = 'createTrBagg numTarget numSource jobStart [numJobs]'
+  usage = 'createTrBagg [--balanced] numTarget numSource jobStart [numJobs]'
+  balanced = False
+  if '--balanced' in args:
+    args.remove('--balanced')
+    balanced = True
   if (len(args) < 3) or (len(args) > 4):
     print >>sys.stderr,usage
     sys.exit(1)
@@ -34,11 +49,11 @@ def main(args=sys.argv[1:]):
     students = getUniqueStudents()
     if studentInd >= len(students):
       return
-    
-    numInstances = (len(students) - 1) * numSource + numTarget
-    probs = [numSource / float(numInstances) for s in students]
-    probs[studentInd] = numTarget / float(numInstances)
-    #print 'probs:',probs,sum(probs)
+    if balanced:
+      probs = calcBalancedProbs(students,numSource,numTarget,studentInd)
+    else:
+      probs = calcProbs(students,numSource,numTarget,studentInd)
+    print 'probs:',probs,sum(probs)
 
     counts = [0 for s in students]
     for i in range(numTarget):
@@ -73,7 +88,10 @@ def main(args=sys.argv[1:]):
               arffFile.write(line)
       print 'removing trial step'
       #removeTrialStep(arffFilename,arffFilenameFilt)
-      name = 'trBagg-%s-%i' % (students[studentInd],classifierInd)
+      if balanced:
+        name = 'trBagg-balanced-%s-%i' % (students[studentInd],classifierInd)
+      else:
+        name = 'trBagg-%s-%i' % (students[studentInd],classifierInd)
       makeTree(arffFilename,True,None,outputBase,name,[],False,1.0)
       #makeTree(arffFilenameFilt,True,None,outputBase,'trBagg-%s-%i' % (students[studentInd],classifierInd),[],False,1.0)
     finally:
